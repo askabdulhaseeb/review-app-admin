4ae8bee30a08efb81d252c4d6699d891da9c43e4
Muhammad Ali Raza <ali.flutter.dev@gmail.com>
2021-05-18 00:08:07 +0500

    updated plugins versions in yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:admin_review/notifications/push_notification.dart';
 import 'package:admin_review/sign_in/sign_in_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -6,9 +5,6 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //ejaz
-  await PushNotificationsManager().init();
-
   runApp(MyApp());
 }
 
